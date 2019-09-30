178812944acf5b2020040c779423a3d771d3a9b7
Mustafa M <mus-m@outlook.com>
2019-09-29 23:35:02 -0400

    typo

--- bin/install.ps1
+++ bin/install.ps1
@@ -52,8 +52,6 @@ if ($julia_version -ge [Version]"1.3") {
     Start-Process -FilePath $julia_installer -ArgumentList "/S /D=$julia_path" -NoNewWindow -Wait
 }
 
-Start-Process -FilePath $julia_installer -ArgumentList "/S /D=$julia_path" -NoNewWindow -Wait
-
 if ($env:APPVEYOR_REPO_NAME -match "(\w+?)\/(\w+?)(?:\.jl)?$") {
     $projectname = $matches[2]
 }
